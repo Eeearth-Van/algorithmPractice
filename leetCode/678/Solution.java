b564ea8fc57346c2eb66f2f7f14584a54dc4a9eb
汪垚垚 <biantaila@hotmail.com>
2019-05-28 16:23:50 +0800

    678 complete

---
+++ leetCode/678/Solution.java
@@ -0,0 +1,51 @@
+package com.live2d.wy;
+
+/**
+ * @program: wy
+ * @package:
+ * @description:
+ * @author: Kevin
+ * @create: 2019-05-28 13:21
+ **/
+class Solution1 {
+    /**
+     *
+     * @param s
+     * @return
+     */
+    public boolean checkValidString(String s) {
+        int l = 0, star = 0, len = s.length();
+        if (len == 0) return true;
+        for (int i = 0; i < len; i++) {
+            if ('(' == (s.charAt(i))) {
+                l++;
+            } else if ('*' == (s.charAt(i))) {
+                star++;
+            } else {
+                l--;
+                //右括号太多
+                if (l + star < 0) return false;
+            }
+        }
+        l = 0;
+        star = 0;
+        for (int i = len - 1; i >= 0; i--) {
+            if (')' == (s.charAt(i))) {
+                l++;
+            } else if ('*' == (s.charAt(i))) {
+                star++;
+            } else {
+                l--;
+                //右括号太多
+                if (l + star < 0) return false;
+            }
+        }
+        return true;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(new Solution1().checkValidString(""));
+    }
+}
