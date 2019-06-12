f39c7697654fe9cddbe6404b883a2e200f3c1b4c
汪垚垚 <biantaila@hotmail.com>
2019-06-12 19:59:35 +0800

    递归

---
+++ leetCode/GenerateParentheses_22_medium/Solution22.java
@@ -0,0 +1,46 @@
+package GenerateParentheses_22_medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @program: leetCode
+ * @package:
+ * @description:
+ * @author: Kevin
+ * @create: 2019-06-11 14:27
+ **/
+
+class Solution22Test {
+    public static void main(String[] args) {
+        Solution22 solution22 = new Solution22();
+        int n = 1;
+        List<String> strings = solution22.generateParenthesis(n);
+        for (String s : strings) {
+            System.out.println(s);
+        }
+    }
+}
+
+public class Solution22 {
+
+    public List<String> generateParenthesis(int n) {
+        if (n <= 0) return new ArrayList<>();
+        List<String> stringList = new ArrayList<String>();
+        generate("", stringList, n, n);
+        return stringList;
+    }
+
+    void generate(String string, List<String> stringList, int left, int right) {
+        if (left < 0 || right < 0) return;
+        if (left == 0 && right == 0) {
+            stringList.add(string);
+            return;
+        }
+        if (left <= right) {
+            String s = new String(string);
+            generate(string + "(", stringList, left-1, right);
+            generate(string + ")", stringList, left, right-1);
+        }
+    }
+}
